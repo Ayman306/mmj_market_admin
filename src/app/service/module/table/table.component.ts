df26ac38de9998b269ce9a8fb566b7b3f8f3e57a
Ayman <aymanuddin1111.au@gmail.com>
2024-09-08 20:57:01 +0530

    Feature:- table pagination configured

--- src/app/service/module/table/table.component.ts
+++ src/app/service/module/table/table.component.ts
@@ -9,6 +9,7 @@ import {
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-table',
@@ -19,6 +20,7 @@ import { MatSlideToggleChange } from '@angular/material/slide-toggle';
     NgClass,
     NgStyle,
     MatSlideToggleModule,
+    MatTooltipModule
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
@@ -27,18 +29,27 @@ export class TableComponent {
   @Input() displayedColumns: string[] | undefined;
   @Input() dataSource: any | undefined;
   @Input() total: number | undefined;
-  @Input() pageSize: number | undefined;
+  @Input() pageSize: number = 10;
   @Output() pageChange = new EventEmitter<PageEvent>();
   @Output() editRecordData = new EventEmitter<any>();
   @Output() statusUpdate = new EventEmitter<any>();
+  @Output() displayService = new EventEmitter<any>();
 
   objectKeys = Object.keys;
   onPageChange(event: PageEvent) {
     this.pageChange.emit(event);
   }
+
   editRecord(data: any) {
     this.editRecordData.emit(data);
   }
+
+  serviceDetail(data: any) {
+    const value = JSON.parse(JSON.stringify(data));
+    value.readonly = true
+    this.displayService.emit(value)
+  }
+
   onStatusChange(item: any, event: MatSlideToggleChange) {
     item.status = event.checked;
     this.statusUpdate.emit(item);
