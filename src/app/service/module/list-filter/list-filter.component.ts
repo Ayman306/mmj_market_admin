9b1ea63db534eb19d0510e9230d3b844551e2fc2
Ayman <aymanuddin1111.au@gmail.com>
2024-09-08 20:57:25 +0530

    Feature:- filter records

--- src/app/service/module/list-filter/list-filter.component.ts
+++ src/app/service/module/list-filter/list-filter.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
+import { debounceTime, Subject } from 'rxjs';
 @Component({
   selector: 'app-list-filter',
   standalone: true,
@@ -17,10 +18,36 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './list-filter.component.html',
   styleUrl: './list-filter.component.scss',
 })
-export class ListFilterComponent {
-  @Input() filterOption: any;
-  search = '';
-  searchData() {
-    console.log(this.search);
+export class ListFilterComponent implements OnInit {
+  ngOnInit(): void {
+    this.searchSubject.pipe(debounceTime(500)).subscribe((searchText) => {
+      this.logSearch(searchText);
+    });
+  }
+  filterOption: any = [
+    { label: 'Anytime', value: 'anytime' },
+    { label: 'Past 24 hours', value: '24 hours' },
+    { label: 'Past weeek', value: 'week' },
+    { label: 'Past month', value: 'month' },
+    { label: 'Active', value: 'active' },
+    { label: 'Inactive', value: 'inactive' },
+  ];;
+  @Output() search = new EventEmitter<any>();
+  @Output() filter = new EventEmitter<any>();
+  private searchSubject: Subject<string> = new Subject();
+
+
+  onSearch(event: any) {
+    const searchText = event.target.value;
+    this.searchSubject.next(searchText);
+
+  }
+  logSearch(text: any) {
+    this.search.emit(text);
+    console.log(text);
+  }
+  onSelect(event: any) {
+    this.filter.emit(event);
+    console.log(event);
   }
 }
